84280037dfc8dcdbcc3cd1ba450e5296c23e2d78
Thantham (Judge) Madan <judge.madan@gmail.com>
2016-02-24 21:06:43 +0700

    Fixed & Optimized Login Form
    Used POST

--- editor/pass.php
+++ editor/pass.php
@@ -1,32 +1,20 @@
-<?php if(getallheaders()["Pass"]!="isbeatz"): ?>
+<?php 
+	if($_POST){
+		$pw = $_POST['password'];
+		if($pw == 'isbeatz'){
+			setcookie("magic_word","softdev1234",time()+315360000,"/","",0);
+			header("Location: index.php");
+		}
+	}
+	
+?>
 <head>
 	<title>Password Page</title>
-	<script Language="Javascript">
-	function post(href,key,value){
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("POST", href, false);
-		xmlhttp.setRequestHeader(key, value);
-		xmlhttp.send();
-		return xmlhttp.responseText;
-	}
-	function submitPass(form){
-		var pass = form.pass.value;
-		if(pass != ""){
-			post("pass.php","pass",pass);
-			window.location = "index.php";
-		}
-		return false;
-	}
-	</script>
 </head>
 <body style="text-align:center">
-	<form onSubmit="return submitPass(this)">
-		Password: <input type="password" name="pass">
+	<form action="pass.php" method="post">
+		Password: <input type="password" name="password">
 		<input type="submit" value="Submit">
 	</form>
 </body>
-<?php else: ?><?php
-setcookie("magic_word","softdev1234",time()+315360000,"/","",0);
-header("Location: index.php");
-?>
-<?php endif ?>
+
